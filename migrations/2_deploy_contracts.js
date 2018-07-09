ff6e40e06ff2a617452d31c374d80e6cfda36337
victor <victor@quikfox.com>
2018-07-09 14:44:56 +0300

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,6 @@
+var CURToken = artifacts.require("./CURToken.sol");module.exports=function(deployer){deployer.deploy(CURToken);};
+
+
+
+
+
---
+++ migrations/3_deploy_contracts.js
@@ -0,0 +1,6 @@
+var CUR = artifacts.require("./CUR.sol");module.exports=function(deployer){deployer.deploy(CUR);};
+
+
+
+
+
